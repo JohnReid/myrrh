55cfa656096e17cb624643b292d479752eff9b84
JohnReid <johnbaronreid@netscape.net>
2013-06-03 11:05:30 +0100

    Fix for inner class registration check

--- myrrh/python/registry.h
+++ myrrh/python/registry.h
@@ -16,15 +16,21 @@ namespace myrrh {
 namespace python {
 
 /**
- * Queries if the the type specified is in the registry.
+ * Queries if the the type specified is in the registry. This can be used to avoid registering
+ * the same type more than once.
  */
 template< typename QueryT >
-bool
-in_registry() {
+const boost::python::converter::registration *
+get_registration() {
 	namespace bp = boost::python;
-	bp::type_info info = boost::python::type_id< QueryT >();
-	bp::converter::registration * registration = boost::python::converter::registry::query( info );
-	return registration != 0;
+	const bp::type_info info = boost::python::type_id< QueryT >();
+	const bp::converter::registration * registration = boost::python::converter::registry::query( info );
+	// need to check for m_to_python converter: see http://stackoverflow.com/a/13017303/959926
+	if( registration != 0 && registration->m_to_python != 0 ) {
+		return registration;
+	} else {
+		return 0;
+	}
 }
 
 } //namespace myrrh
